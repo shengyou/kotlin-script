f13bc4822ccda0ecff6790e0cee216e72c8faa79
Shengyou Fan <shengyoufan@gmail.com>
2020-04-23 17:39:50 +0800

    Scenario: download files (done)

--- 5-DownloadFiles.kts
+++ 5-DownloadFiles.kts
@@ -3,3 +3,10 @@
 import java.net.URL
 import java.nio.file.Files
 import java.nio.file.Paths
+
+for (i in 1..10) {
+    println("Donwloading $i file...")
+    val stream = URL("https://picsum.photos/200/200").openStream()
+    val path = Paths.get("downloads/Image-$i.jpg")
+    Files.copy(stream, path)
+}
