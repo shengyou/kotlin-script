c6363755573e7cf1dcdf2537bb3943037e0774b8
Shengyou Fan <shengyoufan@gmail.com>
2020-04-23 17:41:31 +0800

    Scenario: parse file (done)

--- 6-ParseFile.kts
+++ 6-ParseFile.kts
@@ -3,3 +3,8 @@
 import java.io.File
 
 // print column 9, 0, 11
+val lines = File("data/some_flights.tsv").readLines()
+lines.forEach { row ->
+    val columns = row.split("\t")
+    println("${columns[9]}\t${columns[0]}\t${columns[11]}")
+}
