a3793a312cbfc2463eec0ef55378c928754eb0cb
Shengyou Fan <shengyoufan@gmail.com>
2020-04-23 17:35:56 +0800

    Scenario: delete files older than (done)

--- 4-DeleteFilesOlderThan.kts
+++ 4-DeleteFilesOlderThan.kts
@@ -1,3 +1,8 @@
 #!/usr/bin/env kotlinc -script
 
 import java.io.File
+
+val files = File("files").listFiles { file -> file.isFile }
+val time = System.currentTimeMillis() - 1 * 24 * 60 * 60 * 100
+
+files?.filter { it.lastModified() < time }?.forEach { it.delete() }
