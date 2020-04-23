d730a620466b62527473e84d204b75311e46f904
Shengyou Fan <shengyoufan@gmail.com>
2020-04-23 17:36:15 +0800

    Scenario: delete files older than (with args)

--- 4-DeleteFilesOlderThan.kts
+++ 4-DeleteFilesOlderThan.kts
@@ -2,7 +2,7 @@
 
 import java.io.File
 
-val files = File("files").listFiles { file -> file.isFile }
-val time = System.currentTimeMillis() - 1 * 24 * 60 * 60 * 100
+val files = File(args[0]).listFiles { file -> file.isFile }
+val time = System.currentTimeMillis() - args[1] * 24 * 60 * 60 * 100
 
 files?.filter { it.lastModified() < time }?.forEach { it.delete() }
